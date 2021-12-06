758b7d66e2ca110cfbc1ee385f30b3e741487acc
Bbaker1989 <86622638+Bbaker1989@users.noreply.github.com>
2021-12-05 19:17:48 -0500

    updated css to post listing

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  res.render('homepage', {
+    id: 1,
+    first_name: 'Darla',
+    last_name: 'Beauty',
+    service_id: 1,
+    city_id: 1,
+    link_url:'https://handlebars.js.com/guide/',
+    user: {
+      username: 'test_user'
+    }
+  });
 });
 
 module.exports = router;
