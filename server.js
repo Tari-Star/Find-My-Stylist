52bba9f0666ddf4312eb22c0f21e7a8161d8f4ff
Tari <bota.maral@gmail.com>
2021-12-07 13:54:12 -0500

    add session

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
-const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const session = require("express-session");
+const exphbs = require("express-handlebars");
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const exphbs = require('express-handlebars');
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: process.env.DB_SESSION_SECRET,
+  cookie: { maxAge: 70000 },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  })
+};
+
+app.use(session(sess));
+
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
@@ -16,8 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
-app.use(routes);
+app.use(require("./controllers/"));
 
 
 sequelize.sync({ force: false }).then(() => {
