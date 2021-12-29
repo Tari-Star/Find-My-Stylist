ff9bc4f5173c9176bf2699ab123bad81538872bc
Tari <bota.maral@gmail.com>
2021-12-28 21:13:53 -0500

    update style.css, update homepage.List stylists on homepage.Add partials

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -2,51 +2,31 @@ const { Comment } = require('../models');
 
 const commentData = [
     {
-        comment_text:'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.',
-        post_id: 8,
-        user_id: 5
-    },
-    {
-        comment_text:'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.',
-        post_id: 4,
-        user_id: 4
-    },
-    {
-        comment_text:'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.',
-        post_id: 6,
+        comment_text:'Amolestiae consequatur.',
+        post_id: 5,
         user_id: 1
     },
     {
-        comment_text:'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.',
+        comment_text:'Esse quam nihil molestiae consequatur.',
+        post_id: 4,
+        user_id: 2
+    },
+    {
+        comment_text:'Qlit esse quam nihil molestiae consequatur.',
+        post_id: 3,
+        user_id: 5
+    },
+    {
+        comment_text:'Qoluptate velit esse quam nihil molestiae consequatur.',
         post_id: 2,
         user_id: 3
     },
     {
-        comment_text:'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.',
+        comment_text:'Quis autem vel eum iure reprehenderit .',
         post_id: 1,
-        user_id: 2
+        user_id: 4
     }, 
-    {
-        comment_text:'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.',
-        post_id: 7,
-        user_id: 5
-    },
 ];
-const seedComment = () => Comment.bulkCreate(commentData);
+const seedComments = () => Comment.bulkCreate(commentData);
 
-module.exports = seedComment;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = seedComments;
--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,7 @@
-
-const seedUsers = require('./user-seeds');
 const seedStylists = require('./stylist-seeds');
-const seedComments = require('./comment-seeds');
+const seedUsers = require('./user-seeds');
 const seedPosts = require('./post-seeds');
-
+const seedComments = require('./comment-seeds');
 
 const sequelize = require('../config/connection');
 
@@ -11,17 +9,17 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n------- DATABASE SYNCED -------\n');
 
+  await seedStylists();
+  console.log('\n------- STYLISTS SEEDED -------\n');
+
   await seedUsers();
   console.log('\n------- USERS SEEDED -------\n');
-
-  await seedComments();
-  console.log('\n------- COMMENTS SEEDED-------\n');
 
   await seedPosts();
   console.log('\n------- POSTS SEEDED -------\n');
 
-  await seedStylists();
-  console.log('\n------- STYLISTS SEEDED -------\n');
+  await seedComments();
+  console.log('\n------- COMMENTS SEEDED-------\n');
 
   process.exit(0);
 };
